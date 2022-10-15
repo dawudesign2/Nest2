0d94b5ac98787c00c214d526fe5d94db3478daaa
Dawudesign <contact@dawudesign.fr>
2022-10-15 10:40:36 +0200

    report and user Intity

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/entity/users.entity';
+import { Report } from './reports/entity/report.entity';
 
 @Module({
   imports: [
@@ -17,7 +18,7 @@ import { User } from './users/entity/users.entity';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [User],
+      entities: [User, Report],
       synchronize: true,
     }),
     UsersModule,
--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReportsController } from './controller/reports.controller';
 import { ReportsService } from './service/reports.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Report } from './entity/report.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Report])],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
