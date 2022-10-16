06fc1a53740e31bd7cf0c18a4f3c803669c98ea2
Dawudesign <contact@dawudesign.fr>
2022-10-16 19:29:58 +0200

    next step

--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Delete,
+  Param,
+  Patch,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
+import { CreateUserDto } from '../dtos/create-user.dto';
+import { UpdateUserDto } from '../dtos/update-user.dto';
+import { UsersService } from './../service/users.service';
+import * as bcrypt from 'bcrypt';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get()
+  findAlUsers() {
+    return this.usersService.findAll();
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findById(parseInt(id, 10));
+  }
+
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('/all/:email')
+  findUserByEmail(@Param('email') email: string) {
+    return this.usersService.findByEmail(email);
+  }
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto) {
+    const { email, password } = body;
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+    return this.usersService.create(email, hashedPassword);
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    const { password } = body;
+    if (password) {
+      const saltOrRounds = 10;
+      const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+      return this.usersService.update(parseInt(id, 10), {
+        password: hashedPassword,
+      });
+    }
+    return this.usersService.update(parseInt(id, 10), body);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id, 10));
+  }
+}
--- src/users/entity/users.entity.ts
+++ src/users/entity/users.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User {
@@ -6,11 +7,9 @@ export class User {
   id: number;
 
   @Column()
-  name: string;
-
-  @Column()
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 }
